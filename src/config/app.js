dac779412cc4d01074fcc90aa64bb1a9c356fcf9
jatinpanchal1o1 <jatin1o1panchal@gmail.com>
2026-09-07 11:23:14 -0700

    update-files

---
+++ src/config/app.js
@@ -0,0 +1,7 @@
+export const APP_CONFIG = {
+  brandName: "RentPartner",
+  whatsappNumber: "91+9057708005",
+  telegramUrl: "https://t.me/YOUR_TELEGRAM_USERNAME",
+  supportEmail: "rentpartner.in@gmail.com",
+  supportPhone: "+91 9057708005",
+};
